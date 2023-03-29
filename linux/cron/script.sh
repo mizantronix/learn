1b2b32791d605bd41f29da3b205c4f0afa8e8b7b
mizantronix <mizantronix@gmail.com>
2023-03-30 01:38:54 +0700

    cron

---
+++ linux/cron/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "#!/bin/bash" > ~/test-script.sh
+echo "echo \"hello \$(date)\" >> ~/test-file" >> ~/test-script.sh
+
+chmod +x ~/test-script.sh
+
+crontab -l > ~/tmp-cron && echo '*/2 * * * * ~/test-script.sh 2>~/cron_err' >> ~/tmp-cron && crontab ~/tmp-cron
+rm ~/tmp-cron
